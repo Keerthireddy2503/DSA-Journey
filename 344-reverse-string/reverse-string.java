6ab796c963c81baf10f03d828f2a5733eb9f345a
Keerthireddy2503 <24pa1a4536@vishnu.edu.in>
2026-08-16 12:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (10.74%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start<end){
+            swap(s,start++,end--);
+        }
+    }
+    public void swap(char[]s,int i,int j){
+       char temp = s[i];
+       s[i]=s[j];
+       s[j]=temp;
+    }
+}
